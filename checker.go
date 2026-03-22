c29d2903832b7147651c8d030c0294f8f4ff128b
KRYYYYYYYYYYYYYYYYYYY <135027183+KRYYYYYYYYYYYYYYYYYYY@users.noreply.github.com>
2026-03-23 00:36:10 +0400

    Update checker.go

--- checker.go
+++ checker.go
@@ -140,6 +140,7 @@ func CheckVlessL7(cAddr *C.char, cPort int, cUuid *C.char, cSni *C.char, cPbk *C
 	}
 	probeUAs := []string{
 		"Mozilla/5.0 (Linux; Android 13; SM-A336B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.6367.179 Mobile Safari/537.36 happ/3.15.1",
+		"Happ/3.15.1 (com.happproxy; build:xxx; Android 16; Samsung SM-A336B)",
 		"okhttp/4.12.0 v2rayNG/1.12.28",
 	}
 	successHits := 0
