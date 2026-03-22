2a1b35063ae5cbaa9855a75cc591b792b7b90050
KRYYYYYYYYYYYYYYYYYYY <135027183+KRYYYYYYYYYYYYYYYYYYY@users.noreply.github.com>
2026-03-23 02:09:11 +0400

    Update check.py

--- check.py
+++ check.py
@@ -40,8 +40,9 @@ DEFAULT_MOBILE_USER_AGENTS = [
     "Mozilla/5.0 (Linux; Android 14; Pixel 8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36",
     "Mozilla/5.0 (iPhone; CPU iPhone OS 17_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4 Mobile/15E148 Safari/604.1",
     # Ближе к реальному трафику с Samsung A33 + Android-клиентов
-    "Happ/3.15.1 (com.happproxy; Android 16; Samsung SM-A336B)",
+    "Happ/3.15.1 (com.happproxy; build:xxx; Android 16; Samsung SM-A336B)",
     "okhttp/4.12.0 v2rayNG/1.12.28",
+]
 DEFAULT_PROBE_PATHS = ["/", "/generate_204", "/favicon.ico"]
 
 # Подключаем новую библиотеку
